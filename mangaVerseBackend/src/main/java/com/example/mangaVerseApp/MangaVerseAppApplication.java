1c46826372f6f98d509d559979ce505c6a3dd87c
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-12-03 00:02:00 +0000

    inital commit

---
+++ mangaVerseBackend/src/main/java/com/example/mangaVerseApp/MangaVerseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangaVerseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaVerseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaVerseAppApplication.class, args);
+	}
+
+}
---
+++ mangaVerseBackend/src/test/java/com/example/mangaVerseApp/MangaVerseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangaVerseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaVerseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
